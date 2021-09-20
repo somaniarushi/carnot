30a2e04e2428f30e615d0c4105a8e56c7656ae8c
Arushi Somani <arushisomani@berkeley.edu>
2021-09-19 21:01:50 -0700

    css parser draft 1

--- src/css.py
+++ src/css.py
@@ -22,6 +22,13 @@ class Selector:
         self.id = id
         self.classes = classes
 
+    def specificity(self):
+        """
+        Return a numeric value for the specificity of the selector
+        """
+        #TODO: add support for multiple id and multiplpe tag names
+        return 2 + len(self.classes)
+
 class Declaration:
     """
     Contains a dictionary of properties and their values
@@ -34,4 +41,13 @@ class Declaration:
     def add(self, name, value):
         self.values[name] = value
 
+class Color:
+    """
+    Defines a color in a stylesheet.
+    """
+    def __init__(self, r=0, g=0, b=0, a=0):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
 
--- src/css_parser.py
+++ src/css_parser.py
@@ -1,7 +1,37 @@
 from parser import Parser
-from css import Declaration, Selector
+from css import Rule, Declaration, Selector, Color
 
 class CSSParser(Parser):
+    def parse_rules(self):
+        """
+        Parse through a series of rules
+        """
+        rules = []
+        while not self.eof():
+            self.next_whitespace()
+            rules.append(self.parse_rule())
+        return rules
+
+    def parse_rule(self):
+        """
+        Parse a selector/declarations pair of rule.
+        """
+        selectors = self.parse_selectors()
+        declarations = self.parse_decalarations()
+        return Rule(selectors, declarations)
+
+    def parse_selectors(self):
+        """
+        Parse a comma separated list of selectors.
+        """
+        selectors = []
+        while self.has_next() and self.peek() != '}':
+            selectors.append(self.parse_selector())
+            if self.peek() == ',':
+                self.next(); self.next_whitespace()
+        selectors.sort(key=lambda x: x.specificity())
+        return selectors
+
     def parse_selector(self):
         """
         Parses one selector. eg: type#id.class1.class2
@@ -28,6 +58,18 @@ class CSSParser(Parser):
         """
         return self.next_while(is_valid_identifier)
 
+    def parse_decalarations(self):
+        """
+        Parse a list of declarations enclosed within { ... }
+        """
+        declarations = []
+        assert(self.next() == '{')
+        while self.has_next() and self.peek() != '}':
+            self.next_whitespace()
+            declarations.append(self.parse_declaration())
+        assert(self.next() == '}')
+        return declarations
+
     def parse_declaration(self):
         """
         Parse and return one property:value declaration.
@@ -43,6 +85,31 @@ class CSSParser(Parser):
         assert(self.next() == ';')
         return Declaration(name, value)
 
+    def parse_value(self):
+        """
+        Parse and return the value for a property value pair.
+        """
+        c = self.peek()
+        if c.isnumeric():
+            return self.next_while(lambda x: x.isnumeric())
+        elif c == '#':
+            return self.parse_color()
+        else:
+            return self.parse_identifier().name
+
+    def parse_color(self):
+        """
+        Parse and return a color object
+        """
+        def parse_hex(self):
+            """
+            Parse through a hexcode, wrap it base 256, and return the value
+            """
+            s = self.input[self.pos:self.pos+2]
+            self.pos+=2
+            return int(s) % 256
+        assert(self.next() == '#')
+        return Color(self.parse_hex(), self.parse_hex(), self.parse_hex(), self.parse_hex())
 
 def is_valid_identifier(c):
     """
